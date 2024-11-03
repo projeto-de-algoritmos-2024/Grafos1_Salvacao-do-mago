35e8f1793b9763049309c477b2d2f139dda67f5e
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2024-11-03 01:10:38 -0300

    Update images.py
    modifique o caminho das imagens

--- src/images.py
+++ src/images.py
@@ -4,13 +4,13 @@ from constants import TILE_SIZE
 def load_images():
     # Carregando em variáveis as imagens dos elementos
     images = {
-        "mage_img": load_image('assets/mage.png'),
-        "ally_img": load_image('assets/ally.png'),
-        "ogre_img": load_image('assets/ogre.png'),
-        "river_img": load_image('assets/river.png'),
-        "mountain_img": load_image('assets/mountain.png'),
-        "forest_img": load_image('assets/forest.png'),
-        "floor_img": load_image('assets/floor.png')
+        "mage_img": load_image('../assets/mage.png'),
+        "ally_img": load_image('../assets/ally.png'),
+        "ogre_img": load_image('../assets/ogre.png'),
+        "river_img": load_image('../assets/river.png'),
+        "mountain_img": load_image('../assets/mountain.png'),
+        "forest_img": load_image('../assets/forest.png'),
+        "floor_img": load_image('../assets/floor.png')
     }
 
     return images
